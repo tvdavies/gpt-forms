ee3629dd9c052c4b74e4f966f73e378c996e1618
Tom Davies <tom.davies@tes.com>
2023-05-25 23:50:09 +0100

    remove supabase - not needed for now

--- src/env.mjs
+++ src/env.mjs
@@ -7,11 +7,8 @@ export const env = createEnv({
    * Will throw if you access these variables on the client.
    */
   server: {
-    // DATABASE_URL: z.string().url(),
     OPENAI_API_KEY: z.string().min(1),
     CLERK_SECRET_KEY: z.string().min(1),
-    SUPABASE_URL: z.string().url(),
-    SUPABASE_KEY: z.string().min(1),
   },
   /*
    * Environment variables available on the client (and server).
@@ -28,12 +25,9 @@ export const env = createEnv({
    * 💡 You'll get typeerrors if not all variables from `server` & `client` are included here.
    */
   runtimeEnv: {
-    // DATABASE_URL: process.env.DATABASE_URL,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
     CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
     NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
       process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
-    SUPABASE_URL: process.env.SUPABASE_URL,
-    SUPABASE_KEY: process.env.SUPABASE_KEY,
   },
 });
