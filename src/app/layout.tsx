20eaa75d9421c497b3f16f6db819ad2418cd9915
Tom Davies <tom.davies@tes.com>
2023-05-26 09:27:23 +0100

    set up auth with clerk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { TopNav } from "@/components/TopNav";
 import "./globals.css";
 import { Inter } from "next/font/google";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,11 +16,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-        <TopNav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <TopNav />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -1,10 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
 export function TopNav() {
   return (
     <header className="navbar shadow-lg bg-neutral text-neutral-content mb-4">
-      <div className="flex-none px-2">
+      <div className="flex px-2 w-full justify-between">
         <span className="text-lg font-bold uppercase">
           <a href="/">GPT Forms</a>
         </span>
+        <UserButton afterSignOutUrl="/" />
       </div>
     </header>
   );
